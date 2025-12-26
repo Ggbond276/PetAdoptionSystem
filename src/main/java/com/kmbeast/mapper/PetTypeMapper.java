08dd129575efb4e17051e8c1284e8888d7255a90
GGCoder <2103035673@qq.com>
2025-12-26 17:55:42 +0800

    管理员首页后端开发

--- src/main/java/com/kmbeast/mapper/PetTypeMapper.java
+++ src/main/java/com/kmbeast/mapper/PetTypeMapper.java
@@ -2,50 +2,30 @@ package com.kmbeast.mapper;
 
 import com.kmbeast.pojo.dto.PetTypeQueryDto;
 import com.kmbeast.pojo.entity.PetType;
+import com.kmbeast.pojo.vo.ChartVO;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
+/**
+ * 宠物类别持久化接口
+ */
+@Mapper
 public interface PetTypeMapper {
-    /**
-     * 保存宠物类型
-     * @param petType 宠物类型
-     * @return 操作结果
-     */
+
     void save(PetType petType);
 
-    /**
-     * 更新宠物类型
-     * @param petType 宠物类型
-     * @return 操作结果
-     */
     void update(PetType petType);
 
-    /**
-     * 删除宠物类型
-     * @param id 宠物类型主键ID
-     */
     void deleteById(@Param(value = "id") Integer id);
 
-    /**
-     * 查询宠物类型
-     * @param petTypeQueryDto
-     */
-    List<PetType> query(PetTypeQueryDto petTypeQueryDto);
-
-    /**
-     * 查询宠物类型数量
-     * @param petTypeQueryDto
-     * @return 宠物类型数量
-     */
-    Integer queryCount(PetTypeQueryDto petTypeQueryDto);
-
-    /**
-     * 根据名称查询宠物类型
-     * @param name 宠物类型名称
-     * @return PetType 宠物类型实体
-     */
     PetType queryByName(@Param(value = "name") String name);
 
-    PetType queryById(@Param(value = "id") Integer id);
+    List<PetType> query(PetTypeQueryDto petTypeQueryDto);
+
+    Integer queryCount(PetTypeQueryDto petTypeQueryDto);
+
+    List<ChartVO> petTypeCount();
+
 }
